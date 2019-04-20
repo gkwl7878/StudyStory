7e626739f95468804032c0d263df1825d4cabbac
gkwl7878 <gkwl7878@naver.com>
2019-04-20 12:41:25 +0900

    cm

--- 03.개발/cbe/src/kr/co/studystory/admin/controller/LoginController.java
+++ 03.개발/cbe/src/kr/co/studystory/admin/controller/LoginController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.support.SessionStatus;
 
 import kr.co.studystory.admin.service.CommonMngService;
 import kr.co.studystory.admin.vo.LoginVO;
@@ -11,11 +12,21 @@ import kr.co.studystory.admin.vo.LoginVO;
 @Controller
 public class LoginController {
 	
+	/**
+	 * 로그인 페이지 띄워주기
+	 * @return
+	 */
 	@RequestMapping(value="/admin/login.do",method=RequestMethod.GET)
 	public String loginPage() {
 		return "admin/login";
 	}
 	
+	/**
+	 * 로그인 처리 (아이디,비밀번호 검증, 다음페이지 전달할 값 구하기)
+	 * @param l_vo
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping(value="/admin/login_proc.do", method=RequestMethod.POST)
 	public String loginProcess(LoginVO l_vo, Model model) {
 		String url= "admin/login";
@@ -28,4 +39,16 @@ public class LoginController {
 		model.addAttribute("loginFlag",loginFlag);
 		return url;
 	}
+	
+	/**
+	 * 로그아웃
+	 * @param ss
+	 */
+	@RequestMapping(value="/admin/logout.do")
+	public String LogOut(SessionStatus ss){
+		ss.setComplete();
+		return "admin/login";
+	}
+	
+	
 }
