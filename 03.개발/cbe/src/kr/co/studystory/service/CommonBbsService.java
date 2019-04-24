75ffaddf9bc522c733be00b3704e9d31218569ea
Young Oh <34850791+younggeun0@users.noreply.github.com>
2019-04-24 21:54:02 +0900

    Update 알람

---
+++ 03.개발/cbe/src/kr/co/studystory/service/CommonBbsService.java
@@ -0,0 +1,99 @@
+package kr.co.studystory.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import kr.co.studystory.dao.CommonBbsDAO;
+import kr.co.studystory.domain.NewAlarm;
+import kr.co.studystory.vo.AlaramOrQuestionVO;
+
+@Component
+public class CommonBbsService {
+
+	@Autowired
+	private CommonBbsDAO cb_dao;
+	
+	/**
+	 * 알람, 문의사항 전체 게시글수를 반환하는 메서드
+	 * by 영근 190424
+	 */
+	public int getTotalCnt(AlaramOrQuestionVO aoqvo) {
+		int cnt = cb_dao.selectTotalCnt(aoqvo);
+		return cnt;
+	}
+	
+	/**
+	 * 10개의 게시글을 한 페이지에 보여줄 것
+	 */
+	public int pageScale() {
+		return 10;
+	}
+	
+	/**
+	 * 페이지 수를 구하는 메서드
+	 */
+	public int getTotalPage(int totalCnt) {
+		int totalPage = totalCnt / pageScale();
+		if (totalCnt % pageScale() != 0) {
+			totalPage++;
+		}
+		return totalPage;
+	}
+	
+	/**
+	 * 게시글의 시작번호를 구하는 메서드
+	 */
+	public int beginNum(int currPage) {
+		int begin = 1;
+		begin = currPage * pageScale() - pageScale() + 1;
+		return begin;
+	}
+	
+	/**
+	 * 게시글의 마지막 번호를 구하는 메서드
+	 */
+	public int endNum(int beginNum) {
+		int end = beginNum + pageScale() - 1;
+		return end;
+	}
+	
+	/**
+	 * 페이지 인덱스를 반환하는 메서드 
+	 */
+	public int pageIndexNum() {
+		return 5;
+	}
+	
+	/**
+	 * 시작 페이지 인덱스를 반환하는 메서드
+	 */
+	public int startPage(int currPage, int pageIndexNum) {
+		int startPage = ((currPage - 1)/pageIndexNum) * pageIndexNum + 1;
+		return startPage;
+	}
+	
+	/**
+	 * 마지막 페이지 인덱스를 반환하는 메서드
+	 */
+	public int endPage(int startPage, int pageIndexNum, int totalPage) {
+		int endPage = (((startPage - 1) + pageIndexNum) / pageIndexNum)*pageIndexNum;
+		
+		if (totalPage <= endPage) {
+			endPage = totalPage;
+		}
+		
+		return endPage;
+	}
+	
+	/**
+	 * 새 알람 조회 후 반환
+	 * by 영근 190424
+	 */
+	public List<NewAlarm> getNewAlarms(String id) {
+		return cb_dao.selectNewAlarms(id);
+	}
+	
+	
+}
