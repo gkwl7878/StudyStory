992a77e5376ee880968d2ed1f85c7959ded13334
younggeun0 <34850791+younggeun0@users.noreply.github.com>
2019-05-10 15:41:11 +0900

    Update 암호화처리 수정

--- 03.개발/cbe/src/kr/co/studystory/util/ShaUtil.java
+++ 03.개발/cbe/src/kr/co/studystory/util/ShaUtil.java
@@ -3,32 +3,27 @@ package kr.co.studystory.util;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import org.apache.tomcat.util.codec.binary.Base64;
+
 public class ShaUtil {
     
     public static String shaEncoding(String plainText) {
-          String cipherText = "";
-          
-          if (plainText != null || !"".equals(plainText)) {
-                try {
-                      // 1. 알고리즘을 사용할 수 있는 개체를 얻는다.
-                      MessageDigest md =  MessageDigest.getInstance("SHA-256");
-                      
-                      // 2. 평문(Plain Text)를 암호문(Cipher Text)로  변환
-                      md.update(plainText.getBytes());
-                      
-                      // 3. 변환된 암호문 (Cipher Text) 얻기
-                      cipherText = new String(md.digest());
-                      
-                } catch (NoSuchAlgorithmException e) {
-                      e.printStackTrace();
-                }
-          }
-          
-          return cipherText;
-    }
-    
-    public static void main(String[] args) {
-		System.out.println(shaEncoding("tjdwn12Q"));
-    	
+		String cipherText = "";
+		Base64 base64 = new Base64();
+		
+		if (plainText != null || !"".equals(plainText)) {
+			try {
+				// 1. 알고리즘을 사용할 수 있는 개체를 얻는다.
+				MessageDigest md = MessageDigest.getInstance("SHA-1");
+				// 2. 평문(Plain Text)를 암호문(Cipher Text)로 변환 
+				md.update(plainText.getBytes());
+				// 3. 변환된 암호문 (Cipher Text) 얻기
+				cipherText = new String(base64.encode(md.digest()));
+				
+			} catch (NoSuchAlgorithmException e) {
+				e.printStackTrace();
+			}
+		}
+		return cipherText;
 	}
 }
